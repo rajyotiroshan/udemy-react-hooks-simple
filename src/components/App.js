6e3129dc837b8ef43c7d5f962903d2e4fcc6f510
rajan <rajangreedge@gmail.com>
2019-04-25 08:42:29 +0530

    class based a component toggle todos and posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import
 
 class App extends React.Component {
-  
+  state= {
+    resource: 'posts'
+  };
   render(){
     return (
       <div className="App">
-          App
+          <div>
+            <button onClick={()=>this.setState({resource:'posts'})}>Posts</button>
+            <button onClick={()=>{this.setState({resource:'todos'})}}>Todos</button>
+          </div>
+          {this.state.resource}
       </div>
     );
   }
